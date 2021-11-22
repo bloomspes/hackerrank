84d259a11da61b7754f6607fdc71018a8b3f8934
Spes <bloomspes@gmail.com>
2021-11-22 22:01:09 +0900

    코드 설명 추가.

--- comparator/src/main/java/FilterApple.java
+++ comparator/src/main/java/FilterApple.java
@@ -7,6 +7,8 @@ public class FilterApple {
     public static final Color GREEN = Color.GREEN;
     public enum Color { RED, GREEN }
 
+    // 원시적 형태의 코드 weight, color를 선택하는 메소드가 분리 됨.
+
     public static List<Apple> filterApplesByWeight(List<Apple> inventory, int weight) {
 
         List<Apple> result = new ArrayList<>();
@@ -32,6 +34,8 @@ public class FilterApple {
         return result;
     }
 
+    // ApplePredicate 를 사용해서 파라미터를 동작하는 함수로 넘겨줌.
+
     public interface ApplePredicate {
         boolean test(Apple apple);
     }
@@ -51,6 +55,8 @@ public class FilterApple {
         }
     }
 
+    // Predicate를 외부로 extracts 한 코드.
+
     public static<T> List<T> filter(List<T> list, Predicate<T> predicate)  {
         List<T> result = new ArrayList<>();
 
@@ -61,6 +67,8 @@ public class FilterApple {
         }
         return result;
     }
+
+    // Predicate + RxJava 버전 작성법
 
     public static void filterWithComparator (List<Apple> inventory) {
         inventory.sort(Comparator.comparing(Apple::getWeight));
