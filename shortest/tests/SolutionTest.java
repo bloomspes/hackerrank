8174aab6fbbb53c8328f5bf29185e104d21f6cb2
Spes <bloomspes@gmail.com>
2021-12-15 00:28:49 +0900

    add testcase for return shortest.

--- shortest/tests/SolutionTest.java
+++ shortest/tests/SolutionTest.java
@@ -1,4 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SolutionTest {
-  
+    private static Solution s;
+    private static final String[] testcase =
+            {
+                    "aabbaccc",
+                    "ababcdcdababcdcd",
+                    "abcabcdede",
+                    "abcabcabcabcdededededede",
+                    "xababcdcdababcdcd"
+            };
+
+    private static final int[] result = {7, 9, 8, 14, 17};
+
+    @BeforeEach
+    void setUp() {
+        s = new Solution();
+    }
+
+    @Test
+    void solution() {
+        assertEquals(s.solution(testcase[0]), result[0]);
+    }
 }
